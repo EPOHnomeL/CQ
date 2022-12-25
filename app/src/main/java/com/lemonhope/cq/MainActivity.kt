f2593e3fcab7bb58500aded0e17185e0a6e0ee42
EPOHnomeL <lemonjonathanvorster@gmail.com>
2022-12-25 17:11:51 +0200

    Favourite Tab working, All fragments working finally

--- app/src/main/java/com/lemonhope/cq/MainActivity.kt
+++ app/src/main/java/com/lemonhope/cq/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_topic, R.id.navigation_search
+                R.id.navigation_home, R.id.navigation_favourite, R.id.navigation_topic, R.id.navigation_search
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
